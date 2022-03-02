68e58ede998387cad6d7585d431a744f90be5679
Юля Круглова <juliakruglovka@gmail.com>
2022-03-02 15:25:19 +0300

    Создан проект и подключен bootstrap

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
+// const path = require("path");
+
+
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+
+
+module.exports = {
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title:
+        "Weather Forecast",
+    },
+  }
+}
