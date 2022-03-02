19562ce3914270c24c3abfde880140f2d25e4bdd
Юля Круглова <juliakruglovka@gmail.com>
2022-03-02 18:37:05 +0300

    шаблон

---
+++ src/components/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="container">
+  <div class="icon sun-shower">
+    <div class="cloud"></div>
+    <div class="sun">
+      <div class="rays"></div>
+    </div>
+    <div class="rain"></div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IconComponent',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
